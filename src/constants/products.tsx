0be33fcc9d392b937d90613e8251854eb6b9fd8f
Chris Nguyen <chrisnguyen5@gmail.com>
2024-10-05 13:39:28 -0700

    update project

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -17,29 +17,19 @@ export const products = [
     href: "https://www.yamaha.com/us/yvn/",
     title: "Yamaha YVN",
     description:
-      "A design and development studio that focuses on building quality apps.",
+      "A landing page that caters to targeted audience.",
     thumbnail: yam,
     images: [yam],
     stack: ["Bootstap"],
-    slug: "aceternity",
+    slug: "yamaha",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+        Competely designed and built the YVN website from concept to end-to-end product. 
+        Site was mocked-up in Photoshop and executed with hand-coded Html5/CSS3/jQuery/Bootstrap. 
+        Site is intended to target wholesale dealers.{" "}
         </p>
-        <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
-        </p>{" "}
+        
       </div>
     ),
   },
@@ -47,29 +37,21 @@ export const products = [
     href: "http://staging1.rowdydesign.com/clients/boeing/flight-services-catalog/current/flight-services/",
     title: "Boeing Flight Catalog",
     description:
-      "Practice for technical interviews with hands on coding challenges.",
+      "Online catalog for training and maintenance.",
     thumbnail: boeing,
     images: [boeing],
     stack: ["CustomJS", "Custom CSS"],
-    slug: "algochurn",
+    slug: "boeing",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+        My goal for this project was to create an interactive experience for internal 
+        Boeing employees to consume data. The purpose of this site was to replicate a 
+        native app experience so that pilots can digitally search a platform without 
+        having to travel with suitcases full of hard copy manuals. Assets and front-end 
+        development were built from the ground up.{" "}
         </p>
-        <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
-        </p>{" "}
+        
       </div>
     ),
   },
@@ -77,29 +59,20 @@ export const products = [
     href: "https://www.beachbody.com",
     title: "Beachbody",
     description:
-      "Never write from scratch again with Moonbeam, your AI first writing tool",
+      "Redesigned E-commerce platform",
     thumbnail: bb,
     images: [bb],
     stack: ["Nextjs", "Tailwindcss"],
-    slug: "moonbeam",
+    slug: "beachbody",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+        I brainstormed with a group of designers to help create a fluid and responsive 
+        E-commerce redesign. We approached every angle in user experience to help build 
+        a quick and simple guest checkout. User Experience, assets, and site-map were 
+        built from the ground up.{" "}
         </p>
-        <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
-        </p>{" "}
+        
       </div>
     ),
   },
@@ -107,29 +80,19 @@ export const products = [
     href: "https://vimeo.com/33738006",
     title: "Lexus CT app",
     description:
-      "A beautiful and comprehensive Tailwind CSS components library for building modern websites and applications.",
+      "A beautiful and comprehensive iPad app.",
     thumbnail: ct,
     images: [ct],
     stack: ["Nextjs", "Tailwindcss"],
-    slug: "tailwindmasterkit",
+    slug: "lexus",
     content: (
       <div>
         <p>
-          Sit eiusmod ex mollit sit quis ad deserunt. Sint aliqua aliqua ullamco
-          dolore nulla amet tempor sunt est ipsum. Dolor laborum eiusmod
-          cupidatat consectetur velit ipsum. Deserunt nisi in culpa laboris
-          cupidatat elit velit aute mollit nisi. Officia ad exercitation laboris
-          non cupidatat duis esse velit ut culpa et.{" "}
+        I was very fortunate to be part of the launch of the first Lexus CT iPad app. 
+        My involvement was assisting in designing and laying out the composition of 
+        countless pages for this app.{" "}
         </p>
-        <p>
-          Exercitation pariatur enim occaecat adipisicing nostrud adipisicing
-          Lorem tempor ullamco exercitation quis et dolor sint. Adipisicing sunt
-          sit aute fugiat incididunt nostrud consequat proident fugiat id.
-          Officia aliquip laborum labore eu culpa dolor reprehenderit eu ex enim
-          reprehenderit. Cillum Lorem veniam eu magna exercitation.
-          Reprehenderit adipisicing minim et officia enim et veniam Lorem
-          excepteur velit adipisicing et Lorem magna.
-        </p>{" "}
+        
       </div>
     ),
   },
